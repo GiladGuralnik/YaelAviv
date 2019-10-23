2422aeb4221141fbccb9e303cb3b454df78d770e
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-10-23 22:35:54 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home } from './Home';
 import { About } from './About';
+import { Contact } from './Contact';
 import { Layout } from './components/Layout';
 import { NavigationBar } from './components/NavigationBar';
 
@@ -17,6 +18,7 @@ class App extends Component {
               <Route exact path="/" component={Home}/>
               <Route path="/home" component={Home}/>
               <Route path="/about" component={About}/>
+              <Route path="/contact" component={Contact}/>
             </Switch>
           </Router>
         </Layout>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,51 +1,37 @@
 import React from 'react';
-import { Nav, Navbar,NavDropdown, Form } from 'react-bootstrap';
-import styled from 'styled-components';
+import { Nav, Navbar,NavDropdown } from 'react-bootstrap';
 
-const Styles = styled.div`
-  .navbar {
-    background-color: #222;
-  }
-  a, .navbar-brand, .navbar-nav .nav-link {
-    color: #bbb;
-    &:hover {
-      color: white;
-    }
-  }
-`;
 
 
 export const NavigationBar = () => (
-    <Styles>
-    <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">
-      <img
+    <Navbar bg="navbar navbar-light bg-light">
+    <Navbar.Brand href="home">
+      {/* <img
         alt=""
         src="/logo.png"
         width="50"
         height="50"
-        className="d-inline-block align-top"
-      />
-      {' YAEL AVIV '}
+        className="d-inline-block"
+        /> */}
+        {' YAEL AVIV '}
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-        <Nav.Link href="home">Home</Nav.Link>
-        <Nav.Link href="about">About</Nav.Link>
-        <NavDropdown title="Gallery" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Wood</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Concrete</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Drawing</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">All</NavDropdown.Item>
-        </NavDropdown>
+        <Nav className="mr-auto ">
+            <NavDropdown title="Gallery" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Wood</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Concrete</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Drawing</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">All</NavDropdown.Item>
+            </NavDropdown>            
         </Nav>
-        <Form inline>
-        </Form>
+        <Nav>
+            <Nav.Link class="Button" href="about">About</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+        </Nav>
     </Navbar.Collapse>
     </Navbar>
-    </Styles >
   )
 
 
