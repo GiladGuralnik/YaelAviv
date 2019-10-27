dab427a24e2629d7a4a9d086c63e7e85b70daa70
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-10-27 22:43:08 +0200

    test

--- src/components/Test2.js
+++ src/components/Test2.js
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
-import FsLightbox from 'fslightbox-react';
-import img1 from '../images/1.JPG'
+// import React, { useState } from 'react';
+// import FsLightbox from 'fslightbox-react';
+// import img1 from '../images/1.JPG'
 
 
-function App() {
-// if toggler is updated when lightbox is closed it will open it
-// if toggler is updated when lightbox is opened it will close it
-const [toggler, setToggler] = useState(false);
+// function App() {
+// // if toggler is updated when lightbox is closed it will open it
+// // if toggler is updated when lightbox is opened it will close it
+// const [toggler, setToggler] = useState(false);
 
 
   
-return (
+// return (
     
 
     
 
-    <>
+//     <>
 
-    <a 
+//     <a 
             
-            href="#"
-            onClick={()=>setToggler(!toggler)}
+//             href="#"
+//             onClick={()=>setToggler(!toggler)}
         
         
-        >                
-            <img 
+//         >                
+//             <img 
             
-                src={img1}
-                alt={"img1"}
+//                 src={img1}
+//                 alt={"img1"}
 
-                style={{height:"32%",width:"32%"}}
+//                 style={{height:"32%",width:"32%"}}
             
-            />
-        </a>
+//             />
+//         </a>
 
-    <button onClick={ () => setToggler(!toggler) }>
-    Toggle Lightbox
-    </button>
-    <FsLightbox
-        toggler={ toggler }
-        sources={ [
-            '../../images/3.JPG',
-            '../images/2.JPG'
-        ] }
+//     <button onClick={ () => setToggler(!toggler) }>
+//     Toggle Lightbox
+//     </button>
+//     <FsLightbox
+//         toggler={ toggler }
+//         sources={ [
+//             '../../images/3.JPG',
+//             '../images/2.JPG'
+//         ] }
 
         
-    />
-    </>
-);
-}
+//     />
+//     </>
+// );
+// }
 
-export default App;
+// export default App;
