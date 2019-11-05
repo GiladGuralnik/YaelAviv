a4639d15662b220ba414ce44f53ba2dc902eeb79
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-11-05 22:24:13 +0200

    test for background image

--- src/components/Mixed.js
+++ src/components/Mixed.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react'    
-import img1 from '../images/1.JPG'
-import img2 from '../images/2.JPG'
-import img3 from '../images/3.JPG'
+import img1 from '../images/1.jpg'
+import img2 from '../images/2.jpg'
+import img3 from '../images/3.jpg'
+import img4 from '../images/4.jpg'
+import img5 from '../images/5.jpg'
+import img6 from '../images/6.jpg'
 import { Lightbox } from "react-modal-image";
 
 
@@ -56,22 +59,22 @@ export default class Mixed extends Component{
                             onClick={() => this.openLightbox(img3,"Image 3")}
                         />            
                         <img 
-                            src={img2}
-                            alt={"img2"}
+                            src={img4}
+                            alt={"img4"}
                             style={{height:"32%",width:"32%"}}
-                            onClick={() => this.openLightbox(img2,"Image 2")}
+                            onClick={() => this.openLightbox(img4,"Image 4")}
                         />             
                         <img 
-                            src={img3}
-                            alt={"img3"}
+                            src={img5}
+                            alt={"img5"}
                             style={{height:"32%",width:"32%"}}
-                            onClick={() => this.openLightbox(img3,"Image 3")}
+                            onClick={() => this.openLightbox(img5,"Image 5")}
                         />
                         <img 
-                            src={img1}
-                            alt={"img1"}
+                            src={img6}
+                            alt={"img6"}
                             style={{height:"32%",width:"32%"}}
-                            onClick={() => this.openLightbox(img1,"Image 1")}
+                            onClick={() => this.openLightbox(img6,"Image 6")}
                         />
                 </div>
             </div>
